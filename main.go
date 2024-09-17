84133336b7db6e2724c5f7d9961c637f5baa10f3
pratulgupta2007 <pratulgupta2007@gmail.com>
2024-09-18 02:20:32 +0530

    Added read file functionalily where file content is stored in variables

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Word Frequency Analysis Report")
+	s1 := readfile("./sample1.txt")
+	s2 := readfile("./sample2.txt")
+	s3 := readfile("./sample3.txt")
+	fmt.Println(s1, s2, s3)
+
+}
+
+func readfile(dir string) string {
+	f, err := os.ReadFile(dir)
+	if err != nil {
+		panic((err))
+	}
+	s := string(f)
+	return s
 }
