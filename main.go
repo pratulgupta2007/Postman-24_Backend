39e310c8526ff2ce3dd931ea4920f7e3c7ecf285
pratulgupta2007 <pratulgupta2007@gmail.com>
2024-09-19 03:28:24 +0530

    Comments

--- main.go
+++ main.go
@@ -11,11 +11,13 @@ import (
 
 func main() {
 
+	//Confirming if atleast 1 file path has been given as a command line arg
 	l := len(os.Args)
 	if l == 1 {
 		log.Fatal("No file provided!")
 	}
 
+	//Taking in inputs for exceptions
 	fmt.Printf("Case Sensitive (Y/N): ")
 	var casesens string
 	fmt.Scanln(&casesens)
@@ -24,40 +26,47 @@ func main() {
 	var excludearticles string
 	fmt.Scanln(&excludearticles)
 
+	//Intiliasing word count and histogram-like-map variables
 	totalwordcount := 0
 	var counter = struct {
 		sync.Mutex
 		a map[string]int
 	}{a: make(map[string]int)}
 
+	//Storing all printed info in a long string and printing everything in total and also write it to the file
 	report := "Word Frequency Analysis Report\n----------------------------------\n"
 
+	//Estabilshing Wait Group and iterating through each file provided
 	wg := sync.WaitGroup{}
 	for i := 1; i < l; i++ {
 		wg.Add(1)
+
+		//Creating a go routine: One for each file
 		go func() {
 			defer wg.Done()
 
+			//Reading file, checking error and storing content locally as a string that is further split on the basis of whitespaces
 			f, err := os.ReadFile(os.Args[i])
 			if err != nil {
 				log.Fatal(err)
 			}
 			s := strings.Fields(string(f))
 
+			//Nested switch cases for the 2 exclusions
 			switch casesens {
 			case "Y":
 				switch excludearticles {
 				case "Y":
 					for _, w := range s {
-						w0 := strings.Trim(w, ",.;:?\"'()!")
+						w0 := strings.Trim(w, ",.;:?\"'()!") //Trying to eliminate all possible punctuation
 						w1 := strings.ToLower(w0)
 						if w1 != "a" && w1 != "an" && w1 != "the" {
-							counter.Lock()
-							counter.a[w0] += 1
+							counter.Lock()     //Locking and unlocking the Mutex to prevent
+							counter.a[w0] += 1 //clash between the goroutines
 							counter.Unlock()
 							continue
 						}
-						totalwordcount -= 1
+						totalwordcount -= 1 // Reducing word count because of article being counted
 					}
 				case "N":
 					for _, w := range s {
@@ -100,10 +109,13 @@ func main() {
 		}()
 	}
 	wg.Wait()
+	// Waiting for go routines to complete
 
+	//Calculating number of unique words
 	uqcount := len(counter.a)
 	keys := make([]string, 0, uqcount)
 
+	//Sorting of final map in descending order of frequency
 	for k := range counter.a {
 		keys = append(keys, k)
 	}
@@ -112,12 +124,14 @@ func main() {
 		return counter.a[keys[i]] > counter.a[keys[j]]
 	})
 
+	//Printing top 15 values
 	h := 1
 	for _, k := range keys[:15] {
 		report += fmt.Sprintf("%d. %s: %d\n", h, k, counter.a[k])
 		h++
 	}
 
+	//Adding all final printing info to report and printing it
 	report += "----------------------------------\n"
 	report += fmt.Sprintf("Total unique words: %d\n", uqcount)
 	report += fmt.Sprintf("Total word count: %d\n", totalwordcount)
@@ -125,6 +139,7 @@ func main() {
 
 	fmt.Printf(report)
 
+	//Writing data to file (while overwriting)
 	file, err := os.OpenFile("analysis_report.txt", os.O_CREATE|os.O_WRONLY, 0777)
 	if err != nil {
 		log.Fatal(err)
