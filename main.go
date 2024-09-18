4c2db6c1241b432095ed3296d310a487f7708f1a
pratulgupta2007 <pratulgupta2007@gmail.com>
2024-09-18 18:51:51 +0530

    Added concurrency ability

--- main.go
+++ main.go
@@ -4,27 +4,43 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 )
 
 var dict = map[string]int{}
 
 func main() {
 
-	if len(os.Args) == 1 {
+	l := len(os.Args)
+
+	if l == 1 {
 		log.Fatal("No file provided!")
 	}
 
-	for i := 1; i < len(os.Args); i++ {
-		fmt.Println(readfile(os.Args[i]))
-	}
-
 	fmt.Println("Word Frequency Analysis Report")
-}
 
-func readfile(dir string) string {
-	f, err := os.ReadFile(dir)
-	if err != nil {
-		log.Fatal(err)
+	c := make(chan string)
+
+	go func() {
+
+		wg := sync.WaitGroup{}
+		for i := 1; i < l; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				f, err := os.ReadFile(os.Args[i])
+				if err != nil {
+					log.Fatal(err)
+				}
+				c <- string(f)
+			}()
+		}
+		wg.Wait()
+		close(c)
+	}()
+
+	for i := range c {
+		fmt.Printf(i)
 	}
-	return string(f)
+
 }
